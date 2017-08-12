221f5aa0e0e5269c4e899add3a3a81ce75dac7a7
DarushDev <darushfaqiri@gmail.com>
2017-08-12 20:01:37 +0800

    Add click listener for first screen elements

--- js/app.js
+++ js/app.js
@@ -25,6 +25,50 @@ $(document).ready(function(){
     var playerSymbol = "";
     var isComputerTurn = false;
 
+    $("#computer").click(function(){
+        $("#first-screen h3").eq(1).stop().css('opacity', '0').text("Choose your mark:").animate({
+            opacity: 1
+        }, 1000);
+    });
+
+    $("#human").click(function(){
+        $("#first-screen h3").eq(1).stop().css('opacity', '0').text("Who plays first?").animate({
+            opacity: 1
+        }, 1000);
+    });
+
+    $("#bottom-wrapper i").eq(0).click(function(){
+        $(this).addClass("selected").removeClass("button-hover");
+        $("#bottom-wrapper i").eq(1).addClass("button-hover").removeClass("selected");
+    });
+
+    $("#bottom-wrapper i").eq(1).click(function(){
+        $(this).addClass("selected").removeClass("button-hover");
+        $("#bottom-wrapper i").eq(0).addClass("button-hover").removeClass("selected");
+    });
+
+    playButton.click(function(){
+        $(".button").empty();//clear the screen
+        firstScreen.fadeOut(500).promise().then(function(){//fade out the player chooser screen
+            playScreen.fadeIn(500);//fade in the playing screen
+            if($("#bottom-wrapper i").eq(1).hasClass("selected")){//if cross is selected in the first screen
+                isCircleTurn = false;
+                playerSymbol = "cross";
+                $("#player-turn").addClass("fa-times").removeClass("fa-circle-o");
+            } else { //if circle is selected in the first screen
+                isCircleTurn = true;
+                playerSymbol = "circle";
+                $("#player-turn").addClass("fa-circle-o").removeClass("fa-times");
+            }
+
+            attachClickEvents();
+
+            if( $("#computer").is(":checked") ) {
+                playWithComputer();
+            }
+
+        });
+    });
 
 
 });
